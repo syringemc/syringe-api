c9e312db8668bc33db72db359b0d975c98bb137f
krmn <38986306+krmn08@users.noreply.github.com>
2020-11-17 16:05:19 +0900

    Add PacketID#DISCARD_TEXT

--- src/main/java/org/syringe/api/PacketID.java
+++ src/main/java/org/syringe/api/PacketID.java
@@ -2,6 +2,7 @@ package org.syringe.api;
 
 public final class PacketID {
     public static final String DISPLAY_TEXT = "display_text";
+    public static final String DISCARD_TEXT = "discard_text";
 
     private PacketID() {}
 }
