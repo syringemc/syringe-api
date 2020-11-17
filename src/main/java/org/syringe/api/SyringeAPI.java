4fe639f6fbe4127ad5de83732d810357205ca1d5
krmn <38986306+krmn08@users.noreply.github.com>
2020-11-17 16:02:20 +0900

    Add discardText method

--- src/main/java/org/syringe/api/SyringeAPI.java
+++ src/main/java/org/syringe/api/SyringeAPI.java
@@ -19,4 +19,8 @@ public final class SyringeAPI {
     public static void displayText(UUID uuid, String key, String text, Position position, Style style, float offsetX, float offsetY, float scale, long fadein, boolean shadow) {
         instance.displayText(uuid, key, text, position, style, offsetX, offsetY, scale, fadein, shadow);
     }
+
+    public static void discardText(UUID uuid, String key, float fadeout) {
+        instance.discardText(uuid, key, fadeout);
+    }
 }
